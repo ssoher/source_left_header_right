ed66dade0ccd2ac4efd6a889f0de472d14c0c161
Sarper Soher <sarpersoher@gmail.com>
2020-12-09 23:49:34 +0300

    fixed editor getting stuck changing focus unless I manage to close the last opened file
    plugin no longer checks if the file is a source or a header when that file is focused. it only checks when a file that's not open is opened.

--- src/extension.ts
+++ src/extension.ts
@@ -1,30 +1,36 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('source_left_header_right extension activated');
+    console.log('source_left_header_right extension activated');
 }
 
- vscode.window.onDidChangeActiveTextEditor((editor: vscode.TextEditor | undefined) => {
-    if(undefined || editor == undefined) return;
+// bug: Gets triggered indefinitely since moving the editor to another group changes focus
+/* vscode.window.onDidChangeActiveTextEditor((editor: vscode.TextEditor | undefined) => {
+    if (undefined || editor == undefined) return;
 
     let fn = editor.document.fileName;
-    let ext = fn.substr(fn.lastIndexOf(".")+1);
+    let ext = fn.substr(fn.lastIndexOf(".") + 1);
 
-    if(ext == "c" || ext == "cpp") {
+    console.log(`Opened ${fn}`);
+
+    if (ext == "c" || ext == "cpp") {
         vscode.commands.executeCommand("workbench.action.moveEditorToFirstGroup");
-    } else if(ext == "h" || ext == "hpp") {
+
+        console.log(`Move ${fn} to FIRST group`);
+    } else if (ext == "h" || ext == "hpp") {
+        vscode.commands.executeCommand("workbench.action.moveEditorToLastGroup");
+
+        console.log(`Move ${fn} to LAST group`);
+    }
+}); */
+
+vscode.workspace.onDidOpenTextDocument((doc: vscode.TextDocument) => {
+    let fn = doc.fileName;
+    let ext = fn.substr(fn.lastIndexOf(".") + 1);
+
+    if (ext == "c" || ext == "cpp") {
+        vscode.commands.executeCommand("workbench.action.moveEditorToFirstGroup");
+    } else if (ext == "h" || ext == "hpp") {
         vscode.commands.executeCommand("workbench.action.moveEditorToLastGroup");
     }
 });
-
-// note: For some reason this doesn't work reliably
-/* vscode.workspace.onDidOpenTextDocument((doc: vscode.TextDocument) => {
-    let fn = doc.fileName;
-    let ext = fn.substr(fn.lastIndexOf(".")+1);
-
-    if(ext == "c" || ext == "cpp") {
-        vscode.commands.executeCommand("workbench.action.moveEditorToFirstGroup");
-    } else if(ext == "h" || ext == "hpp") {
-        vscode.commands.executeCommand("workbench.action.moveEditorToLastGroup");
-    }
-}); */
